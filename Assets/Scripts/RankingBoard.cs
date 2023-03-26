1ef5b0e5c6e525005f8d941511de657c2b3eb306
Junho <sdh210202@sdh.hs.kr>
2023-03-26 21:03:58 +0900

    랭킹 버그

--- Assets/Scripts/RankingBoard.cs
+++ Assets/Scripts/RankingBoard.cs
@@ -16,9 +16,17 @@ public class RankingBoard : MonoBehaviour
     [SerializeField] private InputField inputField;
     [SerializeField] private List<Ranker> rankers = new List<Ranker>();
     [SerializeField] private Ranker my = new Ranker();
+    [SerializeField] private GameObject inputNameWnd;
+    [SerializeField] private GameObject rankingWnd;
+    [SerializeField] private Text[] rankerNames;
+    [SerializeField] private Text[] rankerScores;
+    [SerializeField] private Text myName;
+    [SerializeField] private Text myScore;
+
 
     public void UploadRanking(string txt)
     {
+        if (txt.Length < 3) return; 
         my.score = GameManager.Instance.Score;
         my.name = txt;
 
@@ -27,14 +35,30 @@ public class RankingBoard : MonoBehaviour
 
     private void RankingSet()
     {
-        Ranker myIn = my;
+        Ranker myIn = new Ranker();
+        myIn.score = my.score;
+        myIn.name = my.name;
         rankers.Add(myIn);
-        List<Ranker> newList = rankers.OrderByDescending(item => item.score).ToList();
+        rankers = rankers.OrderByDescending(item => item.score).ToList();
 
-        newList.RemoveAt(newList.Count-1);
-        rankers = newList;
+        rankers.RemoveAt(rankers.Count - 1);
+        RankingBoardSet();
     }
 
+    private void RankingBoardSet()
+    {
+        inputNameWnd.SetActive(false);
+        rankingWnd.SetActive(true);
 
+
+        for (int i = 0; i < rankers.Count; i++)
+        {
+            rankerNames[i].text = $"{i + 1}. {rankers[i].name}";
+            rankerScores[i].text = $"{rankers[i].score}";
+        }
+
+        myName.text = my.name;
+        myScore.text = my.score.ToString();
+    }
 
 }
